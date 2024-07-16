8da15c27ca6f7f221b036962ef7c4464e918c361
cwjohn13 <cwjohn13@arizona.edu>
2024-07-15 21:08:08 -0700

    Adding annotations for HW2

--- Intro-master/Intro-master/code/01-Reshape.R
+++ Intro-master/Intro-master/code/01-Reshape.R
@@ -3,8 +3,8 @@
 # library(reshape2)
 
 ## Tidy data
-
-# Values in column names 
+# adding annotations for HW2
+# Values in column names
 
 raw <- read.csv("data/pew.csv", check.names = F)
 
@@ -17,7 +17,7 @@ head(raw)
 head(tidy)
 
 names(tidy) <- c("religion", "income", "n")
-tidy <- melt(raw, id = "religion", 
+tidy <- melt(raw, id = "religion",
   variable.name = "income", value.name = "n")
 
 # Variable names in cells
@@ -26,7 +26,7 @@ raw <- read.delim("data/weather.txt", check.names = F, na.strings = ".")
 
 # Your Turn
 # ------------------------------------------
-raw <- melt(raw, 
+raw <- melt(raw,
   id = c("year", "month", "element"),
   variable.name = "day", na.rm = TRUE)
 raw$day <- as.numeric(as.character(raw$day))
@@ -35,7 +35,7 @@ raw <- raw[, c("year", "month", "day", "element", "value")]
 
 head(raw)
 
-tidy <- dcast(raw, year + month + day ~ element, 
+tidy <- dcast(raw, year + month + day ~ element,
   value.var = "value")
 
 tidy <- dcast(raw, ... ~ element, value.var = "value")
@@ -47,13 +47,13 @@ head(titanic2)
 
 # Your Turn
 # ------------------------------------------
-tidy <- melt(titanic2, id = c("class", "age", "fate"), 
+tidy <- melt(titanic2, id = c("class", "age", "fate"),
   variable.name = "gender")
 head(tidy)
-tidy <- dcast(tidy, class + age + gender ~ fate, 
+tidy <- dcast(tidy, class + age + gender ~ fate,
   value.var = "value")
 head(tidy)
-titanic$rate <- round(titanic$survived / 
+titanic$rate <- round(titanic$survived /
   (titanic$survived + titanic$perished), 2)
 head(tidy)
 # ------------------------------------------
